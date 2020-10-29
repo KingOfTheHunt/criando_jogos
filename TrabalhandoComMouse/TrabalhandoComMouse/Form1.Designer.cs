d855081af930e924f42254d33929c32977c84182
Davi <davifrancisco214@outlook.com>
2020-10-28 21:07:40 -0300

    Colocando o método Inicia dentro de outra thread

--- TrabalhandoComMouse/TrabalhandoComMouse/Form1.Designer.cs
+++ TrabalhandoComMouse/TrabalhandoComMouse/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Trabalhando com o mouse";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- TrabalhandoComMouse/TrabalhandoComMouse/Form1.cs
+++ TrabalhandoComMouse/TrabalhandoComMouse/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrabalhandoComMouse.Componentes;
 
 namespace TrabalhandoComMouse
 {
     public partial class Form1 : Form
     {
+        Painel painel = new Painel();
+
         public Form1()
         {
+            // Adicionando o painel
+            Controls.Add(painel);
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task t = Task.Run(painel.Inicia);
         }
     }
 }
