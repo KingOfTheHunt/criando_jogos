ac4eb6bf528d45d3f2e949be00ef0b6f8b0019db
Davi <davifrancisco214@outlook.com>
2020-10-28 21:33:16 -0300

    Classe que representa um elemento dentro do jogo

---
+++ PrimeiroJogo/PrimeiroJogo/Entidades/Elemento.cs
@@ -0,0 +1,20 @@
+﻿namespace Entidades
+{
+    class Elemento
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Largura { get; private set; }
+        public int Altura { get; private set; }
+        public float Velocidade { get; private set; }
+
+        public Elemento(int x, int y, int largura, int altura, float velocidade)
+        {
+            X = x;
+            Y = y;
+            Largura = largura;
+            Altura = altura;
+            Velocidade = velocidade;
+        }
+    }
+}
